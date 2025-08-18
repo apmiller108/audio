90c0ece6bd4bac5224cbe8a8672aa51819cb4d92
Alex Miller <apmiller108@yahoo.com>
2025-08-17 21:11:20 -0500

    Clean up, add helper functions help and info flags

--- pipewire_mixxx_startup.sh
+++ pipewire_mixxx_startup.sh
@@ -1,125 +1,297 @@
 #!/usr/bin/env bash
 
-# This is a pipewire based setup script to run Mixxx DJ sofware using 4 decks
-# "piped" through Ardour audio channels, then out to an external mixer. Midi
-# control and MIDI clock are included as well. Recording is supported by routing
-# audio from external mixer to Ardour via soundcard.
+# This is a pipewire based setup script to run Mixxx DJ software using 4 decks
+# "piped" through Ardour or Bitwig audio channels. From there the audio is sent
+# out an external mixer. MIDI control and MIDI clock are included as well.
+# Recording is supported by routing the master or record channels from external
+# mixer to whatever recording software via audio interface.
 
 # In the event the volume is very low on a soundcard, use alsamixer or
 # pavucontrol (Pulse Audio Volume Control) to set sound card volume levels.
 
-while getopts r: flag
-do
-  case "${flag}" in
-    r) record=${OPTARG};;
-  esac
+# Default configuration
+DAW=""
+RECORD=""
+SHOW_INFO=false
+
+# Configuration paths
+BITWIG_PROJECT="/home/apmiller/Bitwig Studio/Projects/pmixxx/pmixxx.bwproject"
+ARDOUR_PROJECT="/home/apmiller/mixxx_4_decks_v2"
+RECORDING_PROJECT="/home/apmiller/Recording"
+
+# Device configuration - add your devices here
+declare -A MIDI_DEVICES=(
+    ["Midi Fighter"]="Midi Fighter"
+    ["K2 MIDI"]="K2 MIDI"
+    ["PX5 MIDI"]="PX5 MIDI"
+    ["UMC204HD"]="UMC204HD 192k MIDI"
+    ["Midi Through"]="Midi Through"
+    ["Arduino Leonardo"]="Arduino Leonardo MIDI"
+    ["SQ-1"]="SQ-1 SQ-1 _ CTRL"
+)
+
+# Parse command line arguments
+usage() {
+    cat << EOF
+Usage: $0 [OPTIONS]
+
+Options:
+    --ardour        Use Ardour as the DAW
+    --bitwig        Use Bitwig as the DAW
+    --info          Show MIDI device information and exit
+    -r PROJECT      Start recording project
+    -h, --help      Show this help message
+
+EOF
+}
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --ardour)
+            DAW="ardour"
+            shift
+            ;;
+        --bitwig)
+            DAW="bitwig"
+            shift
+            ;;
+        --info)
+            SHOW_INFO=true
+            shift
+            ;;
+        -r)
+            RECORD="$2"
+            shift 2
+            ;;
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        *)
+            echo "Unknown option: $1" >&2
+            usage
+            exit 1
+            ;;
+    esac
 done
 
+# Utility functions
 find_pw_output_id() {
-  local search_string="$1"
-  pw-link -I -o | grep -E "$search_string" | sed 's/^[[:space:]]*//' | \
-    sed 's/\([[:digit:]]*\).*$/\1/'
+    local search_string="$1"
+    pw-link -I -o | grep -E "$search_string" | sed 's/^[[:space:]]*//' | \
+        sed 's/\([[:digit:]]*\).*$/\1/'
 }
 
 find_pw_input_id() {
-  local search_string="$1"
-  pw-link -I -i | grep -E "$search_string" | sed 's/^[[:space:]]*//' | \
-    sed 's/\([[:digit:]]*\).*$/\1/'
+    local search_string="$1"
+    pw-link -I -i | grep -E "$search_string" | sed 's/^[[:space:]]*//' | \
+        sed 's/\([[:digit:]]*\).*$/\1/'
 }
 
-# create ALSA virtual MIDI ports for internal routing
-sudo modprobe snd-virmidi
+show_midi_info() {
+    echo "=== MIDI OUTPUT DEVICES ==="
+    printf "%-20s %-10s %s\n" "DEVICE NAME" "ID" "FULL NAME"
+    echo "----------------------------------------"
 
-# Device inputs and outputs variables. Find I/O ID from partial device name.
-midi_fighter_out=$(find_pw_output_id "Midi Fighter")
-midi_fighter_in=$(find_pw_input_id "Midi Fighter")
-xonek2_midi_out=$(find_pw_output_id "K2 MIDI")
-xonepx5_midi_out=$(find_pw_output_id "PX5 MIDI")
-xonepx5_midi_in=$(find_pw_input_id "PX5 MIDI")
-umc204_midi_in=$(find_pw_input_id "UMC204HD 192k MIDI")
-umc204_midi_out=$(find_pw_output_id "UMC204HD 192k MIDI")
-midi_thru_in=$(find_pw_input_id "Midi Through")
-midi_thru_out=$(find_pw_output_id "Midi Through")
-mixxx_midi_clock_out=$(find_pw_output_id "Arduino Leonardo MIDI")
-sq1_midi_in=$(find_pw_input_id "SQ-1 SQ-1 _ CTRL")
-virtual_midi_in0=$(find_pw_input_id "VirMIDI\s+.+-0")
-virtual_midi_in1=$(find_pw_input_id "VirMIDI\s+.+-1")
-virtual_midi_in1=$(find_pw_input_id "VirMIDI\s+.+-2")
-virtual_midi_in1=$(find_pw_input_id "VirMIDI\s+.+-3")
-echo $sq1_midi_in
+    for device_key in "${!MIDI_DEVICES[@]}"; do
+        device_pattern="${MIDI_DEVICES[$device_key]}"
+        device_id=$(find_pw_output_id "$device_pattern")
+        full_name=$(pw-link -I -o | grep -E "$device_pattern" | sed 's/^[[:space:]]*//' | head -1)
+        printf "%-20s %-10s %s\n" "$device_key" "${device_id:-N/A}" "${full_name:-Not found}"
+    done
 
-# Start Bitwig Studio (optional)
-pw-link mixxx_midi_clock_out virtual_midi_in0
-pw-link xonepx5_midi_out virtual_midi_in1
-pw-link midi_fighter_out virtual_midi_in2
-bitwig-studio "/home/apmiller/Bitwig Studio/Projects/pmixxx/pmixxx.bwproject" &
-sleep 15
+    echo ""
+    echo "=== MIDI INPUT DEVICES ==="
+    printf "%-20s %-10s %s\n" "DEVICE NAME" "ID" "FULL NAME"
+    echo "----------------------------------------"
 
-# Start Ardour Mixxx project
-# Ardour7 /home/apmiller/mixxx_4_decks_v2 &
-# wait for Ardour to finish loading
-# sleep 15
+    for device_key in "${!MIDI_DEVICES[@]}"; do
+        device_pattern="${MIDI_DEVICES[$device_key]}"
+        device_id=$(find_pw_input_id "$device_pattern")
+        full_name=$(pw-link -I -i | grep -E "$device_pattern" | sed 's/^[[:space:]]*//' | head -1)
+        printf "%-20s %-10s %s\n" "$device_key" "${device_id:-N/A}" "${full_name:-Not found}"
+    done
 
-# Start Mixxx with `--developer` flag to allow mapping to MIDI Through port and verbose logging
-mixxx &
-# Wait for Mixxx to finish loading
-sleep 10
+    echo ""
+    echo "=== VIRTUAL MIDI DEVICES ==="
+    for i in {0..3}; do
+        virt_id=$(find_pw_input_id "VirMIDI\s+.+-$i")
+        virt_name=$(pw-link -I -i | grep -E "VirMIDI\s+.+-$i" | sed 's/^[[:space:]]*//' | head -1)
+        printf "%-20s %-10s %s\n" "VirMIDI-$i" "${virt_id:-N/A}" "${virt_name:-Not found}"
+    done
+}
 
-# Ardour inputs and outputs variables. Find I/O ID from partial device name.
-ardour_midi_clock_in=$(find_pw_input_id "MIDI Clock in")
-ardour_midi_control_in=$(find_pw_input_id "MIDI Control in")
+setup_virtual_midi() {
+    echo "Setting up virtual MIDI ports..."
+    sudo modprobe snd-virmidi
+}
 
-# Clean up default Mixxx to Ardour links. They are not needed.
-# pw-link -d Mixxx:out_0 "ardour:physical_audio_input_monitor_enable"
-# pw-link -d Mixxx:out_1 "ardour:LTC in"
-# pw-link -d Mixxx:out_2 "ardour:Master/audio_in 1"
-# pw-link -d Mixxx:out_3 "ardour:Master/audio_in 2"
-# pw-link -d Mixxx:out_4 "ardour:Deck3/audio_in 1"
-# pw-link -d Mixxx:out_5 "ardour:Deck3/audio_in 2"
-# pw-link -d Mixxx:out_6 "ardour:Deck1/audio_in 1"
-# pw-link -d Mixxx:out_7 "ardour:Deck1/audio_in 2"
-# pw-link -d Mixxx:out_6 "ardour:->D3+B3/audio_return 1"
-# pw-link -d Mixxx:out_7 "ardour:->D3+B3/audio_return 2"
+get_device_ids() {
+    # Get all device IDs
+    midi_fighter_out=$(find_pw_output_id "Midi Fighter")
+    midi_fighter_in=$(find_pw_input_id "Midi Fighter")
+    xonek2_midi_out=$(find_pw_output_id "K2 MIDI")
+    xonepx5_midi_out=$(find_pw_output_id "PX5 MIDI")
+    xonepx5_midi_in=$(find_pw_input_id "PX5 MIDI")
+    umc204_midi_in=$(find_pw_input_id "UMC204HD 192k MIDI")
+    umc204_midi_out=$(find_pw_output_id "UMC204HD 192k MIDI")
+    midi_thru_in=$(find_pw_input_id "Midi Through")
+    midi_thru_out=$(find_pw_output_id "Midi Through")
+    mixxx_midi_clock_out=$(find_pw_output_id "Arduino Leonardo MIDI")
+    sq1_midi_in=$(find_pw_input_id "SQ-1 SQ-1 _ CTRL")
 
-# Setup Mixxx output mapping to Ardour inputs
-# pw-link Mixxx:out_0 "ardour:Deck1/audio_in 1"
-# pw-link Mixxx:out_1 "ardour:Deck1/audio_in 2"
-# pw-link Mixxx:out_2 "ardour:Deck2/audio_in 1"
-# pw-link Mixxx:out_3 "ardour:Deck2/audio_in 2"
-# pw-link Mixxx:out_4 "ardour:Deck3/audio_in 1"
-# pw-link Mixxx:out_5 "ardour:Deck3/audio_in 2"
-# pw-link Mixxx:out_6 "ardour:Deck4/audio_in 1"
-# pw-link Mixxx:out_7 "ardour:Deck4/audio_in 2"
+    # Virtual MIDI devices
+    virtual_midi_in0=$(find_pw_input_id "VirMIDI\s+.+-0")
+    virtual_midi_in1=$(find_pw_input_id "VirMIDI\s+.+-1")
+    virtual_midi_in2=$(find_pw_input_id "VirMIDI\s+.+-2")
+    virtual_midi_in3=$(find_pw_input_id "VirMIDI\s+.+-3")
+}
 
-# Setup Mixxx output mapping to Bitwig inputs
-pw-link Mixxx:out_0 "Bitwig Studio:Mixxx D1_L"
-pw-link Mixxx:out_1 "Bitwig Studio:Mixxx D1_R"
-pw-link Mixxx:out_2 "Bitwig Studio:Mixxx D2_L"
-pw-link Mixxx:out_3 "Bitwig Studio:Mixxx D2_R"
-pw-link Mixxx:out_4 "Bitwig Studio:Mixxx D3_L"
-pw-link Mixxx:out_5 "Bitwig Studio:Mixxx D3_R"
-pw-link Mixxx:out_6 "Bitwig Studio:Mixxx D4_L"
-pw-link Mixxx:out_7 "Bitwig Studio:Mixxx D4_R"
+start_bitwig() {
+    echo "Starting Bitwig Studio..."
+    bitwig-studio "$BITWIG_PROJECT" &
+    echo "Waiting for Bitwig to load..."
+    sleep 15
+}
 
-# Setup MIDI connections
+start_ardour() {
+    echo "Starting Ardour..."
+    Ardour7 "$ARDOUR_PROJECT" &
+    echo "Waiting for Ardour to load..."
+    sleep 15
+}
 
-# Midi Fighter to control effects in Ardour
-pw-link $midi_fighter_out $midi_thru_in
+start_mixxx() {
+    echo "Starting Mixxx..."
+    mixxx &
+    echo "Waiting for Mixxx to load..."
+    sleep 10
+}
 
-# Midi Fighter to control Beebo
-pw-link $midi_fighter_out $umc204_midi_in
-pw-link $umc204_midi_in $umc204_midi_out
+setup_bitwig_audio_routing() {
+    echo "Setting up Bitwig audio routing..."
+    pw-link Mixxx:out_0 "Bitwig Studio:Mixxx D1_L"
+    pw-link Mixxx:out_1 "Bitwig Studio:Mixxx D1_R"
+    pw-link Mixxx:out_2 "Bitwig Studio:Mixxx D2_L"
+    pw-link Mixxx:out_3 "Bitwig Studio:Mixxx D2_R"
+    pw-link Mixxx:out_4 "Bitwig Studio:Mixxx D3_L"
+    pw-link Mixxx:out_5 "Bitwig Studio:Mixxx D3_R"
+    pw-link Mixxx:out_6 "Bitwig Studio:Mixxx D4_L"
+    pw-link Mixxx:out_7 "Bitwig Studio:Mixxx D4_R"
+}
 
-# Mixxx MIDI Clock output
-pw-link $mixxx_midi_clock_out $ardour_midi_clock_in
-pw-link $mixxx_midi_clock_out $xonepx5_midi_in
-pw-link $mixxx_midi_clock_out $umc204_midi_in # To Beebo via UMC in->out above
-pw-link $mixxx_midi_clock_out $sq1_midi_in
+setup_ardour_audio_routing() {
+    echo "Setting up Ardour audio routing..."
+    pw-link Mixxx:out_0 "ardour:Deck1/audio_in 1"
+    pw-link Mixxx:out_1 "ardour:Deck1/audio_in 2"
+    pw-link Mixxx:out_2 "ardour:Deck2/audio_in 1"
+    pw-link Mixxx:out_3 "ardour:Deck2/audio_in 2"
+    pw-link Mixxx:out_4 "ardour:Deck3/audio_in 1"
+    pw-link Mixxx:out_5 "ardour:Deck3/audio_in 2"
+    pw-link Mixxx:out_6 "ardour:Deck4/audio_in 1"
+    pw-link Mixxx:out_7 "ardour:Deck4/audio_in 2"
+}
 
-# For Xonek2 via x-link, which controls some effects in Ardour (eg, filter, external send)
-pw-link $xonepx5_midi_out $midi_thru_in
-pw-link $midi_thru_out $ardour_midi_control_in
+setup_midi_routing() {
+    echo "Setting up MIDI routing..."
 
-if [ -n "$record" ]; then
-  Ardour7 /home/apmiller/Recording &
-fi
+    # Virtual MIDI routing for DAW
+    [[ -n "$virtual_midi_in0" && -n "$mixxx_midi_clock_out" ]] && \
+        pw-link "$mixxx_midi_clock_out" "$virtual_midi_in0"
+    [[ -n "$virtual_midi_in1" && -n "$xonepx5_midi_out" ]] && \
+        pw-link "$xonepx5_midi_out" "$virtual_midi_in1"
+    [[ -n "$virtual_midi_in2" && -n "$midi_fighter_out" ]] && \
+        pw-link "$midi_fighter_out" "$virtual_midi_in2"
+
+    # Midi Fighter routing
+    [[ -n "$midi_fighter_out" && -n "$midi_thru_in" ]] && \
+        pw-link "$midi_fighter_out" "$midi_thru_in"
+    [[ -n "$midi_fighter_out" && -n "$umc204_midi_in" ]] && \
+        pw-link "$midi_fighter_out" "$umc204_midi_in"
+    [[ -n "$umc204_midi_in" && -n "$umc204_midi_out" ]] && \
+        pw-link "$umc204_midi_in" "$umc204_midi_out"
+
+    # MIDI Clock routing
+    if [[ -n "$mixxx_midi_clock_out" ]]; then
+        [[ -n "$xonepx5_midi_in" ]] && pw-link "$mixxx_midi_clock_out" "$xonepx5_midi_in"
+        [[ -n "$umc204_midi_in" ]] && pw-link "$mixxx_midi_clock_out" "$umc204_midi_in"
+        [[ -n "$sq1_midi_in" ]] && pw-link "$mixxx_midi_clock_out" "$sq1_midi_in"
+
+        if [[ "$DAW" == "ardour" ]]; then
+            ardour_midi_clock_in=$(find_pw_input_id "MIDI Clock in")
+            [[ -n "$ardour_midi_clock_in" ]] && pw-link "$mixxx_midi_clock_out" "$ardour_midi_clock_in"
+        fi
+    fi
+
+    # Xone routing for Ardour control
+    if [[ "$DAW" == "ardour" && -n "$xonepx5_midi_out" && -n "$midi_thru_in" && -n "$midi_thru_out" ]]; then
+        pw-link "$xonepx5_midi_out" "$midi_thru_in"
+        ardour_midi_control_in=$(find_pw_input_id "MIDI Control in")
+        [[ -n "$ardour_midi_control_in" ]] && pw-link "$midi_thru_out" "$ardour_midi_control_in"
+    fi
+}
+
+start_recording() {
+    if [[ -n "$RECORD" ]]; then
+        echo "Starting recording project..."
+        Ardour7 "$RECORDING_PROJECT" &
+    fi
+}
+
+# Main execution
+main() {
+    if [[ "$SHOW_INFO" == true ]]; then
+        setup_virtual_midi
+        show_midi_info
+        exit 0
+    fi
+
+    # Validate DAW selection
+    if [[ -z "$DAW" ]]; then
+        echo "Error: Please specify a DAW with --ardour or --bitwig" >&2
+        usage
+        exit 1
+    fi
+
+    echo "Starting DJ setup with $DAW..."
+
+    # Setup virtual MIDI
+    setup_virtual_midi
+
+    # Start DAW
+    case "$DAW" in
+        "bitwig")
+            start_bitwig
+            ;;
+        "ardour")
+            start_ardour
+            ;;
+    esac
+
+    # Start Mixxx
+    start_mixxx
+
+    # Get device IDs
+    get_device_ids
+
+    # Setup audio routing
+    case "$DAW" in
+        "bitwig")
+            setup_bitwig_audio_routing
+            ;;
+        "ardour")
+            setup_ardour_audio_routing
+            ;;
+    esac
+
+    # Setup MIDI routing
+    setup_midi_routing
+
+    # Start recording if requested
+    start_recording
+
+    echo "DJ setup complete!"
+}
+
+# Run main function
+main "$@"
